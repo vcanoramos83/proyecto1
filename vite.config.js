b17d32da6c1eb90455604d50a8d88606acc1dc44
vcanoramos83 <vcanoramos83@gmail.com>
2025-03-11 21:59:22 +0100

    He modificado el archivo vite.config.js y el index.html

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/proyecto1/', // Asegúrate de que el nombre coincide con el repo en GitHub
+  build: {
+    outDir: 'dist',
+  }
+})
